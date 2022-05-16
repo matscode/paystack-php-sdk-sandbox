57dbbddc2e588106999b56fe1a416a315d6471e8
Michael Akanji <matscode@gmail.com>
2022-05-17 00:47:16 +0100

    remove .idea and other vendor files

---
+++ app/Controller/Paystack.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Matscode\Sandbox\Controller;
+
+class Paystack
+{
+    public static function index()
+    {
+        \Flight::view('paystack');
+    }
+}
---
+++ views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('partials.head-content')
+</head>
+<body>
+
+@yield('content')
+
+
+@include('partials.before-body-close')
+</body>
+</html>
